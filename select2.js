dbf34e75c44d62ee7ba2dc0f496d33034bf77d59
Dima Pizhevskii <24640dp@hvhs.school.nz>
2026-06-23 14:42:38 +1200

    working-game-selection

---
+++ select2.js
@@ -0,0 +1,235 @@
+let gameState = "select";
+
+let trackPoints = [];
+let res = 100;
+let seed;
+
+let ballX, ballY;
+let ballVelX = 0;
+let ballVelY = 0;
+let ballR = 14;
+
+let holeX, holeY;
+let holeR = 22;
+
+let dragging = false;
+let pickupX = 0;
+let pickupY = 0;
+
+let level = 1;
+let won = false;
+
+// scoring var
+let strokes = 0;
+let totalScore = 0;
+let winMsg = "";
+let numberWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+}
+
+function initLevel() {
+  seed = random(1000);
+  won = false;
+  strokes = 0;
+  trackPoints = [];
+
+  ballVelX = 0;
+  ballVelY = 0;
+
+  let cx = width / 2;
+  let cy = height / 2;
+  let noiseMax = 1;
+
+  for (let a = 0; a < 360; a += 360 / res) {
+    let xoff = map(cos(a), -1, 1, 0, noiseMax);
+    let yoff = map(sin(a), -1, 1, 0, noiseMax);
+    let r = map(noise(seed + xoff, seed + yoff), 1.1, 0.78, 10, 100);
+
+    let x = cx + r * cos(a);
+    let y = cy + r * sin(a);
+    trackPoints.push(createVector(x, y));
+  }
+
+  holeX = trackPoints[0].x * 0.8 + cx * 0.2;
+  holeY = trackPoints[0].y * 0.8 + cy * 0.2;
+
+  let halfTrack = floor(res / 2);
+  ballX = trackPoints[halfTrack].x * 0.8 + cx * 0.2;
+  ballY = trackPoints[halfTrack].y * 0.8 + cy * 0.2;
+}
+
+
+function draw() {
+  if (gameState === "select") {
+    drawSelect();
+  } else if (gameState === "geodash") {
+    drawGeodash();
+  } else if (gameState === "minigolf") {
+    drawMinigolf();
+  } else if (gameState === "geodash") {
+    drawGeodash();
+  } else if (gameState === "play") {
+    drawGame();
+  } else if (gameState === "gameover") {
+    drawGameOver();
+  }
+}
+
+function drawSelect() {
+  background(34, 100, 34);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  noStroke();
+  textSize(48);
+  text("Select a Game", width / 2, height / 2 -100);
+  textSize(24);
+  text("Press 1 for Geodash, Press 2 for Mini Golf", width / 2, height / 2 -30);
+}
+function drawMinigolf() {
+  background(34, 100, 34);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  noStroke();
+  textSize(48);
+  text("MINI GOLF", width / 2, height / 2 - 40);
+  textSize(24);
+  text("Click to Start", width / 2, height / 2 + 30);
+}
+function drawGeodash() {
+  background(34, 100, 34);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  noStroke();
+  textSize(48);
+  text("Geodash", width / 2, height / 2 - 40);
+  textSize(24);
+  text("Click to Start", width / 2, height / 2 + 30);
+}
+function drawGameOver() {
+  background(34, 100, 34);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  noStroke();
+  textSize(48);
+  text("COURSE COMPLETE", width / 2, height / 2 - 40);
+  textSize(24);
+  text("Total Score: " + totalScore + " strokes", width / 2, height / 2 + 10);
+  textSize(16);
+  text("Click to play again", width / 2, height / 2 + 60);
+}
+function drawGame() {
+  background(34, 100, 34);
+
+  // draw track
+  fill(85, 180, 85);
+  stroke(150, 220, 150);
+  strokeWeight(8);
+  beginShape();
+  for (let i = 0; i < trackPoints.length; i++) {
+    curveVertex(trackPoints[i].x, trackPoints[i].y);
+  }
+  for (let i = 0; i < 3; i++) {
+    curveVertex(trackPoints[i].x, trackPoints[i].y);
+  }
+  endShape();
+
+  // draw hole
+  fill(20);
+  noStroke();
+  circle(holeX, holeY, holeR * 2);
+
+  // draw flag
+  stroke(255);
+  strokeWeight(3);
+  line(holeX, holeY, holeX, holeY - 50);
+  fill(220, 50, 50);
+  noStroke();
+  triangle(holeX, holeY - 50, holeX + 28, holeY - 40, holeX, holeY - 30);
+
+  if (!won) {
+    // physics
+    ballVelX *= 0.97;
+    ballVelY *= 0.97;
+
+    // snaps to zero if moving extremely slowly to let the player to hit again
+    if (abs(ballVelX) < 0.05) ballVelX = 0;
+    if (abs(ballVelY) < 0.05) ballVelY = 0;
+
+    ballX += ballVelX;
+    ballY += ballVelY;
+
+    checkEdges();
+
+    // draw ball
+    fill(255);
+    stroke(200);
+    strokeWeight(1);
+    circle(ballX, ballY, ballR * 2);
+
+    // check win
+    if (dist(ballX, ballY, holeX, holeY) < holeR) {
+      won = true;
+      totalScore += strokes;
+
+      // word determinator for hole in...
+      if (strokes <= 10 && strokes > 0) {
+        winMsg = "Hole in " + numberWords[strokes] + "!";
+      } else {
+        winMsg = "Hole in " + strokes + "!";
+      }
+    }
+
+    // slingshot line
+    if (dragging) {
+      stroke(255, 255, 0);
+      strokeWeight(3);
+      line(ballX, ballY, mouseX, mouseY);
+    }
+
+  } else {
+    // win msg
+    textAlign(CENTER, CENTER);
+    fill(255);
+    noStroke();
+    textSize(32);
+    text(winMsg, width / 2, height / 2 - 20);
+    textSize(16);
+    text("Click to play level " + (level + 1), width / 2, height / 2 + 20);
+  }
+
+  // ui
+  textAlign(LEFT, TOP);
+  fill(255);
+  noStroke();
+  textSize(16);
+  text("Level: " + level + " / 20", 20, 20);
+  text("Strokes: " + strokes, 20, 45);
+  text("Total Score: " + totalScore, 20, 70);
+}
+
+// inputs/controls
+function keyPressed() {
+  if (gameState === "select") {
+    if (key === '1') {
+      gameState = "geodash";
+    } else if (key === '2') {
+      window.location.href = "minigolf.html";
+    }
+  }
+}
+function mousePressed() {
+  if (gameState === "geodash") {
+    // do nothing
+        console.log("geodash")
+        window.location.href = "geodash.html";
+  }
+    else if (gameState === "minigolf") {
+
+        console.log("minigolf")
+        window.location.href = "minigolf.html";
+    }
+
+}
